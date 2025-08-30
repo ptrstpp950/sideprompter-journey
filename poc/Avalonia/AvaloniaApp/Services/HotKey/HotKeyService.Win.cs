24b5b7e23bbed85344bb4c7526831c8ddba48b63
Piotrek Stapp <ptrstpp950@gmail.com>
2025-08-30 15:57:23 +0200

    Fix win compilation

--- poc/Avalonia/AvaloniaApp/Services/HotKey/HotKeyService.Win.cs
+++ poc/Avalonia/AvaloniaApp/Services/HotKey/HotKeyService.Win.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace AvaloniaApp
+namespace AvaloniaApp.Services.HotKey
 {
 
     public class HotKeyServiceWindows : IHotKeyService
